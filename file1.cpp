ef10eec8d85bd4c383d5533610715f21f29cf517
keishashendy <shendyarrafli7@gmail.com>
2025-05-13 10:53:11 +0700

    Menambahkan implementasi fungsi main untuk menghitung luas persegi panjang

--- file1.cpp
+++ file1.cpp
@@ -25,3 +25,12 @@ int bangunDatar::hitungKeliling(int l, int p){
     return (2 * p) + (2 * l);
 
 }
+
+int main(){
+    bangunDatar bD;
+    persegiPanjang pP;
+    persegiPanjang &refpP = pP;
+    refpP.panjang = 10;
+    refpP.lebar = 100;
+    cout << bD.hitungLuas(refpP.lebar, refpP.panjang);
+}
