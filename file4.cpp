5dc9d9a75aa5e9edc7260bea71c939d9485b11df
keishashendy <shendyarrafli7@gmail.com>
2025-05-13 11:18:11 +0700

    Menambahkan constructor dengan parameter string untuk nama

--- file4.cpp
+++ file4.cpp
@@ -19,3 +19,17 @@ mahasiswa::mahasiswa(){
 mahasiswa::mahasiswa(int iNim){
     nim = iNim;
 }
+int bangunDatar::hitungLuas(int l, int p){
+    return l * p;
+
+}
+
+int bangunDatar::hitungKeliling(int l, int p){
+    return (2 * p) + (2 * l);
+
+}
+
+mahasiswa::mahasiswa(string iNama){
+    nama = iNama;
+}
+
